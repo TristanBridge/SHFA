30bf4e18cab1f9d2ed9b88e9699ea99eb89769c3
Tristan Bridge <tbridge360@gmail.com>
2023-04-17 16:50:04 +0200

    changed the marker, added clusters

--- my-project/src/components/Map.vue
+++ my-project/src/components/Map.vue
@@ -23,6 +23,9 @@ import Point from 'ol/geom/Point';
 import Icon from 'ol/style/Icon';
 import Style from 'ol/style/Style';
 import {toLonLat} from 'ol/proj';
+import { Cluster } from 'ol/source';
+import { Circle as CircleStyle, Fill, Stroke, Text } from 'ol/style';
+
 
 export default {
   name: 'MapComponent',
@@ -91,11 +94,10 @@ export default {
     })
   });
 
-
       this.iconStyle = new Style({
         image: new Icon({
-          src: 'https://cdn3.iconfinder.com/data/icons/faticons/32/location-01-512.png',
-          scale: 0.05,
+          src: '/interface/assets/marker-gold.svg',
+          scale: 1,
           anchor: [0.5, 1],
           anchorXUnits: 'fraction',
           anchorYUnits: 'fraction'
@@ -116,24 +118,31 @@ export default {
      this.map.addLayer(this.vectorLayer);
 
     // Add 'click' event listener
-this.map.on('click', (event) => {
-  this.map.forEachFeatureAtPixel(event.pixel, (feature) => {
-    const coordinates = feature.getGeometry().getCoordinates();
-    const lonLat = toLonLat(coordinates);
-    const raa_id = feature.get('raa_id');
-    console.log('Clicked coordinates:', lonLat.reverse());
-    console.log('Clicked raa_id:', raa_id);
-    this.clickedRaaId = raa_id;
-    this.$emit('raa-id-selected', raa_id);
-  });
-});
+    this.map.on('click', (event) => {
+      this.map.forEachFeatureAtPixel(event.pixel, (feature) => {
+        // Check if the feature is a cluster
+        if (feature.get('features')) {
+          const featuresInCluster = feature.get('features');
+          // Iterate over the features inside the cluster
+          for (const f of featuresInCluster) {
+            const coordinates = f.getGeometry().getCoordinates();
+            const lonLat = toLonLat(coordinates);
+            const raa_id = f.get('raa_id');
+            console.log('Clicked coordinates:', lonLat.reverse());
+            console.log('Clicked raa_id:', raa_id);
+            this.clickedRaaId = raa_id;
+            this.$emit('raa-id-selected', raa_id);
+          }
+        }
+      });
+    });
 
-this.map.on('moveend', this.filterAndDisplayCoordinates);
+  this.map.on('moveend', this.filterAndDisplayCoordinates);
 
 },
-  updateCoordinates() {
-  // creates the new layer for the pins
-  const vectorSource = new VectorSource({
+
+updateCoordinates() {
+  const pointSource = new VectorSource({
     features: this.coordinates.map(result => {
       const coord = result.coordinates.coordinates;
       const feature = new Feature({
@@ -145,12 +154,49 @@ this.map.on('moveend', this.filterAndDisplayCoordinates);
     })
   });
 
-  this.vectorLayer = new VectorLayer({
-    source: vectorSource
+  const clusterSource = new Cluster({
+    distance: 40, // Adjust this value to control the clustering distance
+    source: pointSource,
   });
 
+  const clusterLayer = new VectorLayer({
+    source: clusterSource,
+    style: this.createClusterStyle,
+  });
+
+  // Remove the previous vectorLayer
+  if (this.vectorLayer) {
+    this.map.removeLayer(this.vectorLayer);
+  }
+
+  // Set the new vectorLayer to the clusterLayer and add it to the map
+  this.vectorLayer = clusterLayer;
   this.map.addLayer(this.vectorLayer);
-}
+},
+
+createClusterStyle(feature) {
+  const size = feature.get('features').length;
+  if (size === 1) {
+    return this.iconStyle; // Return the individual pin style when cluster size is 1
+  } else {
+    const style = new Style({
+      image: new CircleStyle({
+        radius: 10 + Math.min(size, 50) * 0.1,
+        fill: new Fill({ color: '#3399CC' }),
+        stroke: new Stroke({
+          color: '#fff',
+          width: 2,
+        }),
+      }),
+      text: new Text({
+        text: size.toString(),
+        fill: new Fill({ color: '#fff' }),
+      }),
+    });
+
+    return style;
+  }
+},
 
   },
   computed: {
